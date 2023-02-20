f4ba6a663b100a85c9adb9259dba5a1feb45f7d1
achique-luisdan <achique.luisdan@gmail.comm>
2023-02-20 11:32:28 -0400

    fix: :zap: agrega sugerencia de google al robots

--- pages/api/robots.js
+++ pages/api/robots.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.send('User-agent: * \n Allow: /'); // Send your `robots.txt content here
+  res.send('User-agent: Googlebot'); // Send your `robots.txt content here
 }
