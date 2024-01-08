179100c4f00f8477c97abf902ed0f308f3f0cb6f
Edmadriz <eduarddaniel128@gmail.com>
2024-01-08 18:31:25 -0400

    fix: :bug: solucionado representacion de comandos de consola y presentacion de nombre y apellido en el cardAuthor.jsx

--- components/CardAuthor.jsx
+++ components/CardAuthor.jsx
@@ -9,7 +9,7 @@ export default function CardAuthor({ post }) {
           <Image class="circle-photo" src={post.authorPhoto}   alt="Foto del autor del artículo"  width={100} height={100}/>
           <div>
             <div class="card-header">
-              <h4>{post.authorName}e</h4>
+              <h4>{post.authorName}</h4>
               <Link href={post.authorLink} target="_blank">
                 <Image src="/link.svg"   alt="Icono de enlace/link" width={40} height={40}/>
               </Link>
